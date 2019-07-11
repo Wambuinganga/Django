9f440fadddaa64d29a7d41669eb67c663cf372c2
peshy <wambowmagola7@gmail.com>
2019-07-11 19:14:42 +0300

    Installed Gunicorn

---
+++ api/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from student.models import Student
+from .serializers import StudentSerializer      
+from rest_framework import viewsets
+
+
+class StudentViewSet(viewsets.ModelViewSet):
+	queryset=Student.objects.all()
+	serializer_class=StudentSerializer
